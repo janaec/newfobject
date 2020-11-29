11a339a19422c2fc92ba508a99564f0229ae7ee9
email <jjazminclrk@gmail.com>
2020-11-29 11:46:22 -0500

    adding server.js folder

--- database.sql
+++ database.sql
@@ -9,19 +9,21 @@ id INT NOT NULL auto_increment PRIMARY KEY,
 first_name VARCHAR(30),
 last_name VARCHAR(30),
 role_id INTEGER,
-manager_id INTEGER,
-FOREIGN KEY (role_id) REFERENCES role(id),
-FOREIGN KEY (manager_id) REFERENCES role(id)
+manager_id INTEGER
 );
 
-CREATE TABLE role(
+CREATE TABLE roles(
 id INT NOT NULL,
 title VARCHAR(30),
 salary DECIMAL,
-department_id INTEGER,
-FOREIGN KEY (department_id) REFERENCES department (id)
+department_id INTEGER
 );
 
 CREATE TABLE department(
-    id INTEGER NOT NULL auto_increment PRIMARY KEY,
-)
+id INTEGER NOT NULL auto_increment PRIMARY KEY,
+name VARCHAR(30)
+);
+
+select * from department;
+select * from roles;
+select * from employee;
