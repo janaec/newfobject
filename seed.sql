5174686a3cf2b8bb6013a158f8716a48a1e24236
email <jjazminclrk@gmail.com>
2020-11-29 23:40:23 -0500

    adding roles to the database

--- seed.sql
+++ seed.sql
@@ -5,3 +5,16 @@ VALUE ("Information Technology"),
 ("Finance"),
 ("Sales"),
 ("Legal");
+
+INSERT INTO roles (title, salary, department_id)
+VALUE ("Finance Manager",250000, 2),
+("Finance Supervior",150000, 2 ),
+("Infastructure Manager", 250000, 1),
+("Infastructure Supervisor", 200000, 1),
+("Software Engineer", 180000, 1),
+("Sales Supervisor", 100000, 3),
+("Sales Manager", 80000,3),
+("Salesperson", 65000,3),
+("Lawyer",250000,4),
+("paralegal", 90000,4),
+("Law Clerk", 50000,4);
